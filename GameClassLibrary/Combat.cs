5fa92429de0d6f2382e2813ad0513a57a16aa46c
Mallory Milstead <milsteam4144@student.faytechcc.edu>
2018-11-05 14:33:48 -0500

    Added Combat and Random Classes

---
+++ GameClassLibrary/Combat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameClassLibrary
+{
+    public class Combat
+    {
+        public void InitiateCombat(Player player, Enemies enemy) //Add storm to list of enemies
+        {
+            if (enemy.Name != "storm")
+            { 
+                //Add combat code here
+            }
+            else //if the enemy is a storm
+            {
+                //Code here
+            }
+        }
+    }
+}
--- GameClassLibrary/Random.cs
+++ GameClassLibrary/Random.cs
@@ -8,6 +8,15 @@ namespace GameClassLibrary
 {
     public class Random
     {
-        
+        public static System.Random random = new System.Random();
+        public int randomValue;
+
+        public int GetRandom(int min, int max)
+        {
+            randomValue = random.Next(min, max+1);
+            
+            //Return the random value
+            return randomValue;
+        }
     }
 }
